03ac0511c8df5694c9dede220fa548e88e7f95d2
pzayarny <pzayarny@ptsecurity.com>
2024-07-27 19:15:21 +0300

    add files to ansible project

--- data_slave/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-pg_basebackup -R -h $DB_HOST -U $POSTGRES_USER -D $PGDATA -p $DB_PORT -P || :
-
-sed -i 's/^port=.*/port='$DB_REPL_PORT'/g' $PGDATA/postgresql.conf
-
-bash /usr/local/bin/docker-entrypoint.sh -c config_file=$PGDATA/postgresql.conf -c hba_file=$PGDATA/pg_hba.conf
